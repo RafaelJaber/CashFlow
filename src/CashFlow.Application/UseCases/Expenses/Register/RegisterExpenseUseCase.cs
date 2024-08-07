a4faab8550b0c4ebabdc82d032308b9f43727fa0
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-07 01:32:58 -0300

    ✨ (ExpensesController.cs): Adiciona um novo caso de uso para registrar despesas e retorna a resposta adequada 📝 (RegisterExpenseUseCase.cs, ResponseRegisterExpenseJson.cs): Adiciona classes para lidar com o registro de despesas e a resposta JSON correspondente A adição do caso de uso RegisterExpenseUseCase e das classes relacionadas permite ao sistema lidar com o registro de despesas de forma mais estruturada. Isso melhora a organização do código e a separação de responsabilidades, facilitando a manutenção e a escalabilidade do sistema.

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,14 @@
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        //TODO VALIDATIONS
+        
+        return new ResponseRegisterExpenseJson();
+    }
+}
